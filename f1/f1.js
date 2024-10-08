96b589546f36c35175352b3bd719c3a23cec6687
Benceeeeee <turner.bence@blathy.info>
2024-10-08 16:05:52 +0200

    f1

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,17 @@
+const fetch = require('node-fetch'); 
+const APIAddress = 'https://archive-api.open-meteo.com/v1/archive?';
 
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-async function f1 (lat, long, timeStr) {
-    //TODO
-}
+const f1 = async (lat, long, timeStr) => {
+    const url = `${APIAddress}latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&daily=temperature_2m_max,temperature_2m_min&timezone=auto`;
+
+    const data = await fetch(url)
+        .then(res => res.json())
+        .catch(error => {
+            throw new Error('Error fetching data: ' + error.message);
+        });
+
+    const avgTemp = Math.round((data.daily.temperature_2m_max[0] + data.daily.temperature_2m_min[0]) / 2);
+    return avgTemp;
+};
 
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -3,14 +3,22 @@ Készítsd el az előző feladat azon változatát ahol megadjuk az óraszámot 
 Csak az adott órára vonatkozó hőmérséklettel térjen vissza a függvény.
 */
 
+const fetch = require('node-fetch');
 const locations = require('../locations.json');
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const APIAddress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    const str = APIAdress + `latitude=${lat}&longitude=${long}` +
-        `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    const str = `${APIAddress}latitude=${lat}&longitude=${long}` +
+        `&start_date=${timeStr}&end_date=${timeStr}` +
+        `&hourly=temperature_2m&timezone=auto`;
+
     const rArr = await fetch(str)
-        .then(data => data.json());
-    return rArr.hourly.temperature_2m.at(hour);
-}
+        .then(data => data.json())
+        .catch(error => {
+            throw new Error('Error fetching data: ' + error.message);
+        });
+
+    return rArr.hourly.temperature_2m[hour];
+};
+
 module.exports = f2;
